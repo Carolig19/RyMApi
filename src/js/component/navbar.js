2a85631630feaf7207618d9845c389fea887941c
Carolig19 <ciribarrenguerra@gmail.com>
2022-03-27 21:20:01 +0000

    segundo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,29 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-chartreuse">
+		  <a className="navbar-brand" href="#">R&M Portal</a>
+		  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		  </button>
+		  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul className="navbar-nav mr-auto">
+			  <li className="nav-item active">
+				<a className="nav-link" href="#">World <span className="sr-only">(current)</span></a>
+			  </li>
+			  <li className="nav-item">
+				<a className="nav-link" href="#">Portal Gun</a>
+			  </li>
+			  <li className="nav-item">
+				<a className="nav-link disabled" href="#">Squanch</a>
+			  </li>
+			</ul>
+			<div className="buscador float-md-right">
+			  <input className="form-control" type="search" placeholder="Search in portal" aria-label="Search"/>
+			  <button className="portal btn btn-outline-success col-2 text-white" type="submit">🔍︎</button>
 			</div>
+		  </div>
 		</nav>
-	);
-};
+			);
+		};
+export default Navbar;		
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
+import RickAndMorty from "../../img/rick-y-morty-fanart-2020-6344.jpg";
 import "../../styles/home.css";
+import Carrusel from "/workspace/react-hello-webapp/src/js/component/Carrusel.jsx";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+export const Home = () => {
+	const [Personaje, setPersonaje] = useState([]);
+	useEffect(()=>{
+			fetch("https://rickandmortyapi.com/api/character/22")
+			  .then((response) => response.json())
+			  .then((result) => {
+				setPersonaje(result);
+				console.log(result);
+			  })
+			  .catch((error) => console.log("error", error));
+			}, []);
+	return	(<div className="titulo text-center mt-5">
+		<h1>Welcome to Rick&Morty's World</h1>
 		<p>
-			<img src={rigoImage} />
+			<img src={RickAndMorty} />
 		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+<div><Carrusel/></div>
+	</div>)};
